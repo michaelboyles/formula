59254f70b6b7ac42ffb5e100dd750edbe9fc2ad1
Michael Boyles <michael@boyl.es>
2025-02-21 17:13:48 +0000

    feat: start to define public API

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { Form } from "./useForm"
+export { FormField, ObjectField, ArrayField } from "./FormField"
+
+// Hooks
+
+export { useElements } from "./useElements"
+export { useForm } from "./useForm"
+export { useFormErrors } from "./useFormErrors"
+export { useFormValue } from "./useFormValue"
+export { useIsSubmitting } from "./useIsSubmitting"
+export { useIsTouched } from "./useIsTouched"
+export { useSubmissionError } from "./useSubmissionError"
+
+// Components
+
+export { Checkbox } from "./Checkbox"
+export { Input, Props as InputProps } from "./Input"
+export { IntegerInput, Props as IntegerInputProps } from "./IntegerInput"
+export { Select } from "./Select"
