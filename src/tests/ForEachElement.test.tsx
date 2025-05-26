41309f6d401eb5046e4c0bba5fcc1a386ad10843
Michael Boyles <michael@boyl.es>
2025-05-26 12:01:06 +0100

    test: add ForEachElement test

---
+++ src/tests/ForEachElement.test.tsx
@@ -0,0 +1,37 @@
+import '@testing-library/jest-dom/vitest';
+import { afterEach, test, expect, describe, it } from 'vitest';
+import { cleanup, render } from "@testing-library/react";
+import { userEvent } from '@testing-library/user-event'
+import { useForm } from "../useForm";
+import { ForEachElement } from "../ForEachElement";
+import { Input } from "../Input";
+
+const user = userEvent.setup();
+
+// https://testing-library.com/docs/react-testing-library/api/#cleanup
+afterEach(() => cleanup());
+
+describe("ForEachElement", () => {
+    it("safely iterates the elements", async () => {
+        function Test() {
+            const form = useForm({
+                initialValues: {
+                    tags: [{ label: "typescript" }]
+                },
+                submit: () => "done",
+            })
+
+            return (
+                <ForEachElement field={form.get("tags")}>
+                {tagField => <Input field={tagField.property("label")} data-testid="input" />}
+                </ForEachElement>
+            )
+        }
+
+        const { getByTestId } = render(<Test />);
+        const input = getByTestId("input");
+        expect(input).toHaveValue("typescript");
+        await user.type(input, "2");
+        expect(input).toHaveValue("typescript2");
+    })
+});
