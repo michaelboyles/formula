143db737e8cff7ed9c92a12db1814bcfdcbc48f2
Michael Boyles <michael@boyl.es>
2025-05-30 18:43:32 +0100

    fix: missing exports

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,27 @@ export type { FormField } from "./FormField.ts"
 
 // Hooks
 
-export { useElements } from "./hooks/useElements.ts"
-export { useForm } from "./hooks/useForm.ts"
 export { useBlurred } from "./hooks/useBlurred.ts"
+export { useElements } from "./hooks/useElements.ts"
 export { useFieldErrors } from "./hooks/useFieldErrors.ts"
 export { useFieldValue } from "./hooks/useFieldValue.ts"
+export { useForm } from "./hooks/useForm.ts"
 export { useIsSubmitting } from "./hooks/useIsSubmitting.ts"
 export { useSubmissionError } from "./hooks/useSubmissionError.ts"
 
 // Components
 
-export { Checkbox } from "./controls/Checkbox.tsx"
+export { Checkbox, type Props as CheckboxProps } from "./controls/Checkbox.tsx"
+export { FileInput, type Props as FileInputProps } from "./controls/FileInput.tsx"
 export { Input, type Props as InputProps } from "./controls/Input.tsx"
 export { IntegerInput, type Props as IntegerInputProps } from "./controls/IntegerInput.tsx"
 export { RadioButton, type Props as RadioInputProps } from "./controls/RadioButton.tsx"
-export { Select } from "./controls/Select.tsx"
-export { TextArea } from "./controls/TextArea.tsx"
+export { Select, type Props as SelectProps } from "./controls/Select.tsx"
+export { TextArea, type Props as TextAreaProps } from "./controls/TextArea.tsx"
 
-export { FieldValue } from "./components/FieldValue.tsx"
-export { ForEachElement } from "./components/ForEachElement.tsx"
+export { DebugField, type Props as DebugFieldProps } from "./components/DebugField.tsx"
+export { FieldErrors, type Props as FieldErrorsProps } from "./components/FieldErrors.tsx"
+export { FieldValue, type Props as FieldValueProps } from "./components/FieldValue.tsx"
+export { ForEachElement, type Props as ForEachElementProps } from "./components/ForEachElement.tsx"
+export { IsSubmitting, type Props as IsSubmittingProps } from "./components/IsSubmitting.tsx"
+export { SubmissionError, type Props as SubmissionErrorProps } from "./components/SubmissionError.tsx"
